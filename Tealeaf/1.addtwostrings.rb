bf1fe3d880d71bf5656d2e01a939b14ed2f25661
lw80 <Contactluke247@gmail.com>
2015-03-21 23:18:42 -0500

    mesg

---
+++ Tealeaf/1.addtwostrings.rb
@@ -0,0 +1,13 @@
+#Add two strings
+
+#Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+
+fname = "luke"
+lname = "wetzel"
+
+puts fname + " " + lname
+#Concatenate
+
+#Interpolate = Insert between fixed points
+puts "#{fname} #{lname}"
+
