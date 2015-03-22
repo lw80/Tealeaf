4ac6d8ce8e7e22781d61ee429908b692f93e99d9
lw80 <Contactluke247@gmail.com>
2015-03-21 23:53:35 -0500

    ,

---
+++ Tealeaf/6.float.rb
@@ -0,0 +1,17 @@
+#6.float.rb 
+#!usr/bin/env/ ruby -wKU
+
+# Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+
+# Float is an integer with a characters after a decimal
+# Put the sqaure of a float integer, NOT SQUARE ROOT
+# Repeat twice more
+# The square of a number is the number times itself
+
+# 
+# Module: BigMath (Ruby 1.9.3)
+# sqrt(x,prec) click to toggle source. Computes the square root of x to the specified number of digits of precision.
+
+puts 4.30 * 4.30
+puts 3333.333 * 3333.333
+puts 1.1 * 1.1
